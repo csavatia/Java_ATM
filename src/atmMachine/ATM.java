c05034ccabd167b7589887b09cd8260d6eff74b3
csavatia <savatiacedric@gmail.com>
2024-06-05 14:21:38 +0300

    first commit

---
+++ src/atmMachine/ATM.java
@@ -0,0 +1,9 @@
+package atmMachine;
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+    public static void main(String[] args) throws IOException  {
+        OptionMenu optionMenu = new OptionMenu();
+        optionMenu.getLogin();
+    }
+}
